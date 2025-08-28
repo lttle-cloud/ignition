c18272c516eb9f3a6dc70195ba35c56498b6d36c
Stefan Ghegoiu <ghegoiustefan@gmail.com>
2025-08-28 18:36:30 +0300

    fix: imports

--- console/src/routeTree.gen.ts
+++ console/src/routeTree.gen.ts
@@ -9,8 +9,8 @@
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
 import { Route as rootRouteImport } from './routes/__root'
-import { Route as IndexRouteImport } from './routes/index'
 import { Route as MachinesRouteImport } from './routes/machines'
+import { Route as IndexRouteImport } from './routes/index'
 
 const MachinesRoute = MachinesRouteImport.update({
   id: '/machines',
