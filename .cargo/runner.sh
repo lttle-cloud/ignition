92ae754005a068a954d0b9cba3e29e219e308787
Laurentiu Ciobanu <laurentiu@lttle.cloud>
2025-09-02 14:47:29 +0000

    set proper caps for mkfs

--- .cargo/runner.sh
+++ .cargo/runner.sh
@@ -5,7 +5,7 @@ BIN_PATH="$1"; shift
 # if the path contains the word "ignition" (lib tests) or "ignitiond" (daemon), we need to set the capabilities
 if [[ "$BIN_PATH" == *"ignition"* || "$BIN_PATH" == *"ignitiond"* ]]; then
     echo "Setting capabilities for $BIN_PATH"
-    sudo setcap 'cap_net_admin+ep cap_dac_override+ep cap_net_bind_service+ep' "$BIN_PATH"
+    sudo setcap 'cap_net_admin+ep cap_dac_read_search,cap_dac_override+ep cap_net_bind_service+ep cap_setpcap+ep' "$BIN_PATH"
 fi
 
 exec "$BIN_PATH" "$@"
