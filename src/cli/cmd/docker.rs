b8da63f7ac50184b013f09a131581e3d3705cf33
Laurentiu Ciobanu <laurentiu@lttle.cloud>
2025-09-10 08:07:01 +0000

    improve ergonomics for docker login

--- src/cli/cmd/docker.rs
+++ src/cli/cmd/docker.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use atty::Stream;
 use clap::Args;
 
 use crate::{client::get_api_client, config::Config, ui::message::message_info};
@@ -11,16 +12,24 @@ pub async fn run_docker_login(config: &Config, _args: DockerLoginArgs) -> Result
     let me = api_client.core().me().await?;
     let registry_robot = api_client.core().get_registry_robot().await?;
 
+    if !atty::is(Stream::Stdout) {
+        print!("{}", registry_robot.pass);
+
+        return Ok(());
+    }
+
     message_info(format!(
         "You are logged in as {} (tenant: {})",
         me.sub, me.tenant
     ));
 
-    message_info(format!("To login to the registry, run the command below:",));
+    message_info(format!(
+        "To login to the registry, pipe the output of this command straight to docker like this:\n",
+    ));
 
     println!(
-        "docker login {} -u {} -p {}",
-        registry_robot.registry, registry_robot.user, registry_robot.pass
+        "lttle docker login | docker login {} -u {} --password-stdin",
+        registry_robot.registry, registry_robot.user
     );
 
     Ok(())
